f2c3670cc5453ee11f0191d441245f45d826fc88
Lev Vyshkvarko <74271247+lvyshkvarko@users.noreply.github.com>
2021-12-17 22:38:01 +0300

    Add files via upload

--- SkyConverter/SkyConverter.py
+++ SkyConverter/SkyConverter.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import getpass
-import tkinter as tk
 from tkinter import filedialog as fd
 
 root = Tk()
@@ -17,18 +16,11 @@ status.place(x=0, y=100)
 status_expectation = Label(root, text='Ожидание загрузки файла', font=("Arial Bold", 20), bg="#FF9999")
 status_expectation.place(x=110, y=100)
 
-button = Button(root, text="Загрузить файл", font=("Arial Bold", 20), bg="lightgreen")
-button.place(relx=0.5, rely=0.5, anchor="c")
-
-
-# Экран загрузки ниже, нужно зафиксить и соединить
 def callback():
     name= fd.askopenfilename()
     print(name)
 
-errmsg = 'Error!'
-tk.Button(text='Click to Open File',
-       command=callback).pack(fill=tk.X)
-tk.mainloop()
+button = Button(root, text="Загрузить файл", font=("Arial Bold", 20), bg="lightgreen", command=callback)
+button.place(relx=0.5, rely=0.5, anchor="c")
 
 root.mainloop()
